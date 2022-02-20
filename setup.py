db86145719d6132c8a3cfcb7d2011bf1aedb5728
Paul Nuyujukian <38359213+bil-paul@users.noreply.github.com>
2022-02-19 17:16:05 -0800

    add license and some pypi formatting

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-import setuptools
+import pathlib, setuptools
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
+
+pwd = pathlib.Path(__file__).parent
+long_description = (pwd / "README.md").read_text()
 
 setuptools.setup(
         name = 'colab_sshfs',
+        version = VERSION,
         description = 'Mount sshfs remotes on Google Colaboratory',
         author = 'Paul Nuyujukian',
-        version = VERSION,
+        url = 'https://github.com/bil/colab_sshfs',
+        long_description = long_description,
+        long_description_content_type='text/markdown',
+        license = 'GPL2',
         packages = setuptools.find_packages() )
